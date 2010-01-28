c496708d6f0d79514e339f2134bec40daf09f77b
R. Kyle Murphy <Kyle@v@gmail.com>
2010-01-27 23:48:20 -0500

    Fixed an issue where = was used in place of ==, and changed the string comparisons to lowercase versions.

--- Project1/Chapter 3 Final Project/Chapter 3 Final Project/Program.cs
+++ Project1/Chapter 3 Final Project/Chapter 3 Final Project/Program.cs
@@ -68,20 +68,23 @@ namespace CHAP_3_END_PROJ
 			Console.WriteLine("What is your quest?: ");
 			quest = Console.ReadLine();
 			   
-			if (quest != "to seek the holy grail") {
+			if (!quest.ToLower().Equals("to seek the holy grail")) {
 				GrailFail();
 				return;
-			} else if (quest = "to seek the holy grail") {
+			} else if (quest.ToLower().Equals("to seek the holy grail")) {
 				Console.WriteLine("What is your favorite color?: ");
 				color = Console.ReadLine();
 				if(color.ToLower().Equals("blue")) {
 					//TODO: Generate random value. If value > some preset kill them, else say correct.
 					GrailWin();
+					return;
 				} else {
 					GrailFail();
+					return;
 				}
 			} else {
 				GrailWin();
+				return;
 			}
 		}
 		
@@ -92,7 +95,7 @@ namespace CHAP_3_END_PROJ
         public static void Main(string[] args) {
 			//Setting up my variables
 			string gameOption = "";
-			while (gameOption != "quit") {
+			while (!gameOption.ToLower().Equals("quit")) {
 	            Console.WriteLine("Welcome to my Chapter Three Game");
 	            Console.WriteLine("Enter the # of the game you want to play, or type \"quit\" to exit:");
 	            Console.WriteLine("1 Elmer Fudd");
